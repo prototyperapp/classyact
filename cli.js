1fd1f33ccf03961cbbb0dcb8a29c24858d3e1be4
Daryl Rowland <daryl@waffleandtoast.com>
2017-08-07 22:21:52 +0100

    Added support for react classes

--- cli.js
+++ cli.js
@@ -10,7 +10,7 @@ var returnHelp = function() {
   console.log("To use, type classyact CLASSNAME");
 };
 
-var createClass = function(currentDir, className) {
+var createNativeClass = function(currentDir, className) {
   var extension = ".js";
   var actualClassName = className;
 
@@ -34,6 +34,31 @@ var createClass = function(currentDir, className) {
   });
 };
 
+
+var createReactClass = function(currentDir, className) {
+  var extension = ".js";
+  var actualClassName = className;
+
+  if (className.indexOf(".") > 0) {
+    extension = "." + className.substr(className.indexOf(".") + 1);
+    actualClassName = className.substr(0, className.indexOf("."));
+  }
+
+  var fileContents = "import React, { Component } from 'react';\n";
+  fileContents += "import Radium from 'radium;\n";
+  fileContents += "\n\nexport default class " + actualClassName + " extends Radium(Component) {\n";
+  fileContents += "\trender() {\n\n\t}\n}";
+  fileContents += "\n\nconst localStyles = {\n\n};"
+
+  fs.writeFile(currentDir + "/" + actualClassName + extension, fileContents, "utf8", function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Created " + currentDir + "/" + process.argv[COMMAND_ARG_INDEX]);
+    }
+  });
+};
+
 var main = function() {
   if (process.argv.length < 3) {
     returnHelp();
@@ -42,7 +67,13 @@ var main = function() {
 
   var command = process.argv[COMMAND_ARG_INDEX];
   var currentDir = sh.pwd();
-  createClass(currentDir, process.argv[COMMAND_ARG_INDEX]);
+
+  // Try and figure out if we're building a React Native class or a React Class
+  if (process.argv.length > 3 && process.argv[3] == "-react") {
+    createReactClass(currentDir, process.argv[COMMAND_ARG_INDEX]);
+  } else {
+    createNativeClass(currentDir, process.argv[COMMAND_ARG_INDEX]);
+  }
 
 };
 
